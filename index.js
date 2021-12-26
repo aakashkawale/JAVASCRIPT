9f2a3339993c5e3df42e482e8ca9f13e82030bad
aakashkawale <aakashkawale007@gmail.com>
2021-12-26 14:55:50 +0530

    these are some codes which i have done while studying js

---
+++ index.js
@@ -0,0 +1,30 @@
+//  //console api
+//  console.log("HELLO JI");
+//  console.warn("abba dabba chabba ");
+//  console.error("error he re");
+
+ //varibles
+ var a = 5;
+ var b = 6;
+ console.log(a+b);
+
+ // data types in js
+ //string
+ var str1 = 'abd';
+ 
+ //number
+ var num1 = 5.6;
+ var num2 = 12;
+
+ //objects 
+ var marks = {
+     raj : 45,
+     nem : 55,
+     mew : 35
+ }
+ console.log(marks);
+
+ // boolen
+ var aa = true;
+ var b = false;
+ console.log(aa)
